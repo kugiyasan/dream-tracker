39f4730fe32eacb8f6a78724afc8c958d35e93fd
kugiyasan <kugiyasan@users.noreply.github.com>
2023-02-04 13:44:01 -0500

    created MaterialModule and project-card cpmponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectCardComponent } from './project-card/project-card.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ProjectCardComponent],
+    imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule, MaterialModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
