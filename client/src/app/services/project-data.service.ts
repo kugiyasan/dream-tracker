231f643b59723146d01e8a3aab03a38932d01bc0
kugiyasan <kugiyasan@users.noreply.github.com>
2023-02-05 02:09:39 -0500

    fixed import path

--- client/src/app/services/project-data.service.ts
+++ client/src/app/services/project-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Project, serverUrl } from './constants';
+import { Project, serverUrl } from '../constants';
 
 @Injectable({
     providedIn: 'root',
