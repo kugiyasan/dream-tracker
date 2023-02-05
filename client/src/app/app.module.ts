6750d39bc850817f94a9159c5d39a7970844e99b
clovisjohn <jonathanyaro24@gmail.com>
2023-02-05 00:03:09 -0500

    build : timeline for posts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,35 @@ import { StatsComponent } from './stats/stats.component';
 import { AllProjectsPageComponent } from './all-projects-page/all-projects-page.component';
 import { AppMaterialModule } from './modules/material.module';
 import {ChartModule} from 'primeng/chart';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from 'primeng/button';
+
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
 
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
     declarations: [AppComponent, ProjectCardComponent, ProjectPageComponent, AllProjectsPageComponent,StatsComponent],
-    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, InfiniteScrollModule,ChartModule],
+    imports: [
+        AppMaterialModule, 
+        AppRoutingModule, 
+        BrowserAnimationsModule, 
+        BrowserModule, 
+        InfiniteScrollModule,
+        ChartModule,
+        InputTextModule,
+        InputTextareaModule,
+        FormsModule,
+        ButtonModule,
+        CommonModule,
+        TimelineModule,
+        CardModule],
+
+
 
     providers: [],
     bootstrap: [AppComponent],
